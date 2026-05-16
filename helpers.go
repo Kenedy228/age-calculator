4d209235c638d9adc1f51d06b0fe0d818f79a574
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-05-16 22:38:18 +1000

    добавил функцию hasHasBirthdayAt для подсчета был на день рождения на указанную дату

--- helpers.go
+++ helpers.go
@@ -1,5 +1,23 @@
 package age_calculator
 
+import "time"
+
+func hasHadBirthdayAt(date, asOf time.Time) bool {
+	if date.Month() == 2 && date.Day() == 29 && !isLeapYear(asOf.Year()) {
+		pseudoDob := time.Date(asOf.Year(), 2, 28, 0, 0, 0, 0, time.UTC)
+		return asOf.After(pseudoDob)
+	}
+
+	if asOf.Month() > date.Month() {
+		return true
+	}
+	if asOf.Month() == date.Month() && asOf.Day() >= date.Day() {
+		return true
+	}
+
+	return false
+}
+
 func isLeapYear(year int) bool {
 	if year%400 == 0 {
 		return true
